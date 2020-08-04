faaf02f25038bec5ea688ca0baf1e13c45dbf76d
root <none@none.com>
2020-08-04 21:58:12 +0000

    day 2

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -2,28 +2,35 @@ import turtle
 
 if __name__ == '__main__':
     window = turtle.Screen()
-    window.bgcolor('white')
+    window.bgcolor('red')
     
     # This code makes a new Turtle. Pick a new name for the turtle
     myTurtle = turtle.Turtle()
     
     # Make your turtle's shape 'turtle', .shape('turtle')
-    
+    myTurtle.shape('turtle')
     # Set your turtle's speed using .speed(2)
-    
+    myTurtle.speed(10)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    myTurtle.color('green')
     # Move your turtle forward using .forward(100)
+    myTurtle.forward(100)
+    myTurtle.left(90)
+    myTurtle.forward(90)
+    myTurtle.left(90)
+    myTurtle.forward(100)
+    myTurtle.left(90)
+    myTurtle.forward(100)
     # TEST    Did your turtle move forward?
-    
+    myTurtle.pencolor('blue')
     # Move your turtle left or right using .left(90) or .right(90)
-    
+
     # Now put the forward and left/right code into a for loop to repeat 4 times.
+
     # TEST    Did your turtle draw a square?
-        
+    myTurtle.width(width=10)
     # Move your turtle to a new place on the screen using .goto(x, y)
     # x=0 and y=0 is the center of the screen
-    
     # Have your turtle draw a circle using .circle(radius, steps=30)
     # TEST    Did your turtle draw a circle?
     
--- _01_drawing/_c_logical_turtle.py
+++ _01_drawing/_c_logical_turtle.py
@@ -31,14 +31,14 @@ if __name__ == '__main__':
     window.setup(width=0.75, height=0.8, startx=0, starty=0)
     
     # 1. Make a new turtle
-    
+    myTurtle = turtle.Turtle()
     # 2. Make your turtle's shape 'turtle', .shape('turtle')
-    
+    myTurtle.shape('turtle')
     # 3. Set your turtle's color using .color('green') and .pencolor('blue')
     
     # 4. Set and new width, length, and outline of our turtle
     #    myTurtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
-
+    myTurtle.turtlesize(stretch_wid=10,stretch_len=10,outline=4)
     # 5. Uncomment the following line and replace 'myTurtle' with your turtle
     #myTurtle.onclick(turtleClicked)
 
